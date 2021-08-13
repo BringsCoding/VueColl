f958501a57a477032d86490843ca4ee44c7d925e
BringsCoding <simonbrings@googlemail.com>
2021-08-13 19:56:38 +0200

    ListIsColor

--- src/components/ClColorlist.vue
+++ src/components/ClColorlist.vue
@@ -1,15 +1,18 @@
 <template>
-    <ul>
-      <li v-for="color in colors" :key="color">{{color}}
-        <ClButton @click="del(color)" textbutton='weg damit'/>
-      </li>
-    </ul>
+  <ul>
+    <li
+      v-for="color in colors"
+      :key="color"
+      :style="{ 'background-color': color }"
+    >
+      {{ color }}
+      <ClButton @click="del(color)" textbutton="weg damit" />
+    </li>
+  </ul>
 </template>
 
 <script>
-
 export default {
-
   props: {
     colors: {
       type: Array,
@@ -18,8 +21,7 @@ export default {
   },
   methods: {
     del(color) {
-      this.$emit('delete', color)
-
+      this.$emit("delete", color);
     },
   },
 };
